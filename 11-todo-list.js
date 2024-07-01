2b11943458bd7694e4db78ee4b3673b883926fb8
Tomáš Križák <tomaskrizak@gmail.com>
2024-07-01 21:45:30 +0200

    change for loop to forEach method

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -26,24 +26,21 @@ function addTodo() {
 function renderTodoList() {
   let todoListHTML = '';
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
+  todoList.forEach(function (todoObject, index) {
     const { name, dueDate } = todoObject;
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
 
     const html = `
     <p>${name}</p>
     <p>${dueDate}</p>
     <button class="delete-todo-button" onclick="
-    todoList.splice(${i}, 1);
+    todoList.splice(${index}, 1);
     localStorage.setItem('todo-list', JSON.stringify(todoList));
     renderTodoList();
 
     ">Delete</button>
     `;
     todoListHTML += html;
-  }
+  });
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
